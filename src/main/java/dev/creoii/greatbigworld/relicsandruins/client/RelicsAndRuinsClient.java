d23dc370815bbdb7698a8b6943be74c9da4009e9
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-03-27 22:54:16 -0400

    Initial commit

---
+++ src/main/java/dev/creoii/greatbigworld/relicsandruins/client/RelicsAndRuinsClient.java
@@ -0,0 +1,10 @@
+package dev.creoii.greatbigworld.relicsandruins.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RelicsAndRuinsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
