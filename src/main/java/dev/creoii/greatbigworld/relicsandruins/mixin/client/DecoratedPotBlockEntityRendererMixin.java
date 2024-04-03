730529a4a10851e904c8d5c41ac6e2bc5ad5d34a
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-04-02 22:18:03 -0400

    Fix decorated pot render error

--- src/main/java/dev/creoii/greatbigworld/relicsandruins/mixin/client/DecoratedPotBlockEntityRendererMixin.java
+++ src/main/java/dev/creoii/greatbigworld/relicsandruins/mixin/client/DecoratedPotBlockEntityRendererMixin.java
@@ -110,7 +110,7 @@ public abstract class DecoratedPotBlockEntityRendererMixin {
 
     @Inject(method = "getSidesTexturedModelData", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private static void gbw$addDecalSidesTexturedModelData(CallbackInfoReturnable<TexturedModelData> cir, ModelData modelData, ModelPartData modelPartData, ModelPartBuilder modelPartBuilder) {
-        ModelPartBuilder decalBuilder = ModelPartBuilder.create().uv(1, 0).cuboid(-01f, 0f, -01f, 141f, 16f, 01f, EnumSet.of(Direction.NORTH));
+        ModelPartBuilder decalBuilder = ModelPartBuilder.create().uv(1, 0).cuboid(-.01f, 0f, -.01f, 14.01f, 16f, .01f, EnumSet.of(Direction.NORTH));
         modelPartData.addChild("back_trim", decalBuilder, ModelTransform.of(15f, 16f, 1f, 0f, 0f, (float) Math.PI));
         modelPartData.addChild("left_trim", decalBuilder, ModelTransform.of(1f, 16f, 1f, 0f, -1.5707964f, (float) Math.PI));
         modelPartData.addChild("right_trim", decalBuilder, ModelTransform.of(15f, 16f, 15f, 0f, 1.5707964f, (float) Math.PI));
@@ -118,8 +118,7 @@ public abstract class DecoratedPotBlockEntityRendererMixin {
         modelPartData.addChild("back_pattern", decalBuilder, ModelTransform.of(15f, 16f, 1f, 0f, 0f, (float) Math.PI));
         modelPartData.addChild("left_pattern", decalBuilder, ModelTransform.of(1f, 16f, 1f, 0f, -1.5707964f, (float) Math.PI));
         modelPartData.addChild("right_pattern", decalBuilder, ModelTransform.of(15f, 16f, 15f, 0f, 1.5707964f, (float) Math.PI));
-        modelPartData.addChild("front_pattern", decalBuilder, ModelTransform.of(1f, 16f, 15f, (float) Math.PI, 0f, 0f));
-    }
+        modelPartData.addChild("front_pattern", decalBuilder, ModelTransform.of(1f, 16f, 15f, (float) Math.PI, 0f, 0f));    }
 
     @Unique
     private void renderTrimmedSide(ModelPart part, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, SpriteIdentifier textureId) {
