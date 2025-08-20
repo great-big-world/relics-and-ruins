65b74e73b082f331206a0d1ca03e1029e8304513
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-08-20 19:56:18 -0400

    Update RelicsAndRuinsClient.java

--- src/main/java/dev/creoii/greatbigworld/relicsandruins/client/RelicsAndRuinsClient.java
+++ src/main/java/dev/creoii/greatbigworld/relicsandruins/client/RelicsAndRuinsClient.java
@@ -2,12 +2,12 @@ package dev.creoii.greatbigworld.relicsandruins.client;
 
 import dev.creoii.greatbigworld.relicsandruins.registry.RelicsAndRuinsBlocks;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class RelicsAndRuinsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getTranslucent(), RelicsAndRuinsBlocks.ICE_BRICKS, RelicsAndRuinsBlocks.ICE_BRICK_STAIRS, RelicsAndRuinsBlocks.ICE_BRICK_SLAB, RelicsAndRuinsBlocks.VERTICAL_ICE_BRICK_SLAB, RelicsAndRuinsBlocks.ICE_BRICK_WALL);
+        BlockRenderLayerMap.putBlocks(BlockRenderLayer.TRANSLUCENT, RelicsAndRuinsBlocks.ICE_BRICKS, RelicsAndRuinsBlocks.ICE_BRICK_STAIRS, RelicsAndRuinsBlocks.ICE_BRICK_SLAB, RelicsAndRuinsBlocks.VERTICAL_ICE_BRICK_SLAB, RelicsAndRuinsBlocks.ICE_BRICK_WALL);
     }
 }
